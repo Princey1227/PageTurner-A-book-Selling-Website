7081ace046d7ba2d372feeb83f36c61243648c77
Princeyadav1227 <princeyadavs7266@gmail.com>
2025-09-21 18:12:56 +0530

    Change in sql

--- shop_db.sql
+++ shop_db.sql
@@ -20,6 +20,7 @@ CREATE TABLE `cart` (
     `name` VARCHAR(255) NOT NULL,
     `price` DECIMAL(10,2) NOT NULL,
     `quantity` INT NOT NULL,
+    `image` VARCHAR(255) NOT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
